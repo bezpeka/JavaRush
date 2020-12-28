3d9914b01fa4d5639311a072b7716514598c968d
Sergey <servitant@gmail.com>
2020-12-28 13:01:39 +0200

    Add class DigitMain

---
+++ src/main/java/com/servitant/TestExample/DigitSumm.java
@@ -0,0 +1,37 @@
+/**
+ * Имеется число. Посчитать сумму цифр данного числа.
+ * Так же делаем на двух циклах и FOR и WHILE
+ */
+
+package com.servitant.TestExample;
+
+public class DigitSumm {
+    public static void main (String[] args) {
+        int value = 12345634;
+
+        int result = sum ( value );
+        System.out.println ( result );
+
+        int result1 = sum ( value );
+        System.out.println ( result1 );
+    }
+
+    public static int sum (int value) {
+        int result = 0;
+        int currentValue = value;
+        while (currentValue != 0) {
+            result += currentValue % 10;
+            currentValue /= 10;
+        }
+        return result;
+    }
+
+    public static int sumFor (int value) {
+        int result = 0;
+        for (int currentValue = value; currentValue != 0; currentValue /= 10) {
+            result = currentValue % 10;
+        }
+        return result;
+    }
+
+}
