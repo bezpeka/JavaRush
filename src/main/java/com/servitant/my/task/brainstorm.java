92fd393ed91cae1606290442526cca70809cd189
Sergey <servitant@gmail.com>
2020-12-31 10:07:41 +0200

    Edit method brainstorm

--- src/main/java/com/servitant/my/task/brainstorm.java
+++ src/main/java/com/servitant/my/task/brainstorm.java
@@ -2,13 +2,17 @@ package com.servitant.my.task;
 
 public class brainstorm{
     public static void main(String[] args){
+        //создаем переменные
         int a=2;
         int b=5;
-        int result = vartest(a, b);
+        int c=8;
+        //вызываем наш метод сложения
+        int result = varplus(a, b, c);
         System.out.println(result);
     }
-    public static int vartest(int var1, int var2){
-        int res = var1 + var2;
+        //создаем функцию (метод) сложения переменных
+    public static int varplus(int var1, int var2, int var3){
+        int res = var1 + var2 + var3;
         return res;
     }
 
