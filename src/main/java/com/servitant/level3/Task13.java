111725064b72062a78e284328cb8d36f5bd16a52
Sergey <servitant@gmail.com>
2021-01-27 16:17:34 +0200

    продолжаю решать задачи JR

---
+++ src/main/java/com/servitant/level3/Task13.java
@@ -0,0 +1,25 @@
+package com.servitant.level3;
+/*
+Вывести на экран все возможные комбинации слов "Мама", "Мыла", "Раму".
+Подсказка: их 6 штук.
+Каждую комбинацию вывести с новой строки. Слова не разделять.
+
+Пример:
+МылаРамуМама
+РамуМамаМыла
+...
+
+ */
+public class Task13{
+    public static void main(String[] args){
+        String a = "Мама";
+        String b = "Мыла";
+        String c = "Раму";
+        System.out.println(a+b+c);
+        System.out.println(b+a+c);
+        System.out.println(c+a+b);
+        System.out.println(b+c+a);
+        System.out.println(c+b+a);
+        System.out.println(a+c+b);
+    }
+}
