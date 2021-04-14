60a22c89adf40bffa06c488b865130546d84350f
Sergey <servitant@gmail.com>
2021-04-14 16:02:21 +0300

    Task8-10

---
+++ src/main/java/com/servitant/level4/Task12.java
@@ -0,0 +1,39 @@
+package com.servitant.level4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Правило треугольника
+package com.javarush.task.task04.task0415;
+
+Ввести с клавиатуры три числа а, b, c - стороны предполагаемого треугольника.
+Определить возможность существования треугольника по сторонам.
+Результат вывести на экран в следующем виде:
+"Треугольник существует." - если треугольник с такими сторонами существует.
+"Треугольник не существует." - если треугольник с такими сторонами не существует.
+
+Подсказка:
+Треугольник существует только тогда, когда сумма двух его сторон больше третьей.
+Требуется сравнить каждую сторону с суммой двух других.
+Если хотя бы в одном случае сторона окажется больше либо равна сумме двух других,
+то треугольника с такими сторонами не существует.
+*/
+
+public class Task12 {
+    public static void main (String[] args) throws Exception {
+        //напишите тут ваш код
+        System.out.println ("Введите с клавиатуры три целых числа");
+        BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
+        int a = Integer.parseInt (reader.readLine ());
+        int b = Integer.parseInt (reader.readLine ());
+        int c = Integer.parseInt (reader.readLine ());
+
+        if ((a + b) > c && (a + c) > b && (c + b) > a){
+            System.out.println ("Треугольник существует.");
+        }
+        else {
+            System.out.println ("Треугольник не существует.");
+        }
+    }
+}
