83a4f4ae35a4f1a97aed1b7d9af4be6ae6032628
Sergey <servitant@gmail.com>
2021-04-14 11:17:27 +0300

    Task3-4

---
+++ src/main/java/com/servitant/level4/Task3.java
@@ -0,0 +1,27 @@
+package com.servitant.level4;
+
+/*
+Хорошо или плохо?
+*/
+public class Task3 {
+    public static void main (String[] args) {
+        compare (3);
+        compare (6);
+        compare (5);
+    }
+
+    public static void compare (int number) {
+        //напишите тут ваш код
+        if (number < 5){
+            System.out.println ("Число меньше 5");
+        }
+        else if (number > 5){
+            System.out.println ("Число больше 5");
+        }
+        else if (number == 5){
+            System.out.println ("Число равно 5");
+        }
+    }
+}
+
+
