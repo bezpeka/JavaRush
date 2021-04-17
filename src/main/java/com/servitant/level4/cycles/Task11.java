68f5dbc97fc2d5beed575d8e426e4a5955b6a033
servitant@gmail.com <Icemin532>
2021-04-17 14:23:34 +0300

    Task11-12

---
+++ src/main/java/com/servitant/level4/cycles/Task11.java
@@ -0,0 +1,16 @@
+package com.servitant.level4.cycles;
+
+/* Достойная оплата труда
+package com.javarush.task.task04.task0440;
+
+Используя цикл вывести на экран сто раз надпись:
+"Я никогда не буду работать за копейки. Амиго"
+Каждое значение вывести с новой строки.
+ */
+public class Task11 {
+    public static void main ( String[] args ) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println ( "Я никогда не буду работать за копейки. Амиго" );
+        }
+    }
+}
