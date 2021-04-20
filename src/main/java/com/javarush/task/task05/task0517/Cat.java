a7cea8eef25d3bcad46afd6c04538151c7c78a9c
Sergey <servitant@gmail.com>
2021-04-20 15:07:30 +0300

    lvl5 task16-18

---
+++ src/main/java/com/javarush/task/task05/task0517/Cat.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task05.task0517;
+
+/*
+Конструируем котиков
+Создать класс Cat (кот) с пятью конструкторами:
+- Имя,
+- Имя, вес, возраст
+- Имя, возраст (вес стандартный)
+- вес, цвет, (имя, адрес и возраст - неизвестные. Кот - бездомный)
+- вес, цвет, адрес (чужой домашний кот)
+
+Задача конструктора - сделать объект валидным.
+Например, если вес не известен, то нужно указать какой-нибудь средний вес.
+Кот не может ничего не весить.
+То же касательно возраста. А вот имени может и не быть (null). То же касается адреса: null.
+*/
+public class Cat {
+    String name = null;
+    int age = 2;
+    int weight = 5;
+    String address = null;
+    String color = "red";
+
+    public Cat (String name) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Cat (String name, int weight, int age) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Cat (String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Cat (int weight, String color) {
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Cat (int weight, String color, String address) {
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ src/main/java/com/javarush/task/task05/task0518/Dog.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task05.task0518;
+
+/*
+Регистрируем собачек
+
+Создать класс Dog (собака) с тремя конструкторами:
+- Имя
+- Имя, рост
+- Имя, рост, цвет
+*/
+
+public class Dog {
+    String name;
+    int height;
+    String color;
+
+    public Dog (String name) {
+        this.name = name;
+    }
+
+    public Dog (String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public Dog (String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
