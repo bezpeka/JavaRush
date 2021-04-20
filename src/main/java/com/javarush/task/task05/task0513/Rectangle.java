38d535af4f69f25fb5112f82678b7f970219dc74
Sergey <servitant@gmail.com>
2021-04-20 14:05:17 +0300

    lvl5 task10-15

---
+++ src/main/java/com/javarush/task/task05/task0513/Rectangle.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task05.task0513;
+
+/*
+Собираем прямоугольник
+
+Создать класс прямоугольник (Rectangle). Его данными будут top, left, width, height
+(верхняя координата, левая, ширина и высота).
+Создать для него как можно больше методов initialize(...)
+
+Примеры:
+- заданы 4 параметра: left, top, width, height
+- ширина/высота не задана (оба равны 0)
+- высота не задана (равно ширине) создаём квадрат
+- создаём копию другого прямоугольника (он и передаётся в параметрах)
+*/
+
+public class Rectangle {
+    //напишите тут ваш код
+    int top;
+    int left;
+    int width = 0;
+    int height = 0;
+
+    public void initialize (int top, int left, int width, int height) {
+        this.top = top; //заданы 4 параметра: left, top, width, height
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+    public void initialize (int top, int left) {
+        this.top = top; //ширина/высота не задана (оба равны 0)
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void initialize (int top, int left, int width ) {
+        this.top = top; //высота не задана (равно ширине) создаём квадрат
+        this.left = left;
+        this.width = width;
+        this.height = width;
+    }
+    public void initialize () {
+        this.top = top; //создаём копию другого прямоугольника (он и передаётся в параметрах)
+        this.left = left;
+        this.width = 10;
+        this.height = 5;
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
