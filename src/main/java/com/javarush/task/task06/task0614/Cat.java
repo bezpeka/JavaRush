94d8720dd9829924f9cf3f8804712874689da776
Sergey <servitant@gmail.com>
2021-04-23 14:25:14 +0300

    lvl6 task13 - 15

---
+++ src/main/java/com/javarush/task/task06/task0614/Cat.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task06.task0614;
+
+import java.util.ArrayList;
+
+/*
+Статические коты
+
+1. В классе Cat добавь публичную статическую переменную cats (ArrayList<Cat>).
+2. Пусть при каждом создании кота (нового объекта Cat) в переменную cats добавляется этот новый кот. Создать 10 объектов Cat.
+3. Метод printCats должен выводить всех котов на экран. Нужно использовать переменную cats.
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    public static ArrayList<Cat> cats = new ArrayList <Cat> ();
+
+    public Cat() {
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 0; i < 10; i++){
+            cats.add (new Cat());
+        }
+        printCats();
+    }
+
+    public static void printCats() {
+        //напишите тут ваш код
+        for (Cat cat : cats){ //WTF - Почитайте о цикле for each.
+            System.out.println (cat);
+        }
+    }
+}
+
---
+++ src/main/java/com/javarush/task/task06/task0618/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task06.task0618;
+                                                  
+/*                                                   
+KissMyShinyMetalAss
+Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
+*/
+
+public class Solution {
+    public static class KissMyShinyMetalAss{
+
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        KissMyShinyMetalAss robot = new KissMyShinyMetalAss();
+        System.out.println(robot);
+    }
+}                                                  
+                                                  
+                             
