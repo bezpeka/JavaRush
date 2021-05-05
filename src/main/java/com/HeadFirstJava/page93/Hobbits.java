2276084c0733abe8b128f96d8fdfb60ed7d26ce2
Sergey <servitant@gmail.com>
2021-05-05 10:58:46 +0300

    HeadFirstJava

--- src/main/java/com/HeadFirstJava/page93/Hobbits.java
+++ src/main/java/com/HeadFirstJava/page93/Hobbits.java
@@ -4,9 +4,9 @@ public class Hobbits {
     String name;
 
     public static void main (String[] args) {
-        Hobbits[] h = new Hobbits[3];
+        Hobbits[] h = new Hobbits[6];
         int z = -1; // Тут важно что массив начинается с нулевого элемента
-        while (z < 2) {
+        while (z < 5) {
             z = z + 1;
             h[z] = new Hobbits ();
             h[z].name = "Бильбо";
@@ -15,6 +15,15 @@ public class Hobbits {
             }
             if (z == 2){
                 h[z].name = "Сэм";
+            }
+            if (z == 3){
+                h[z].name = "Фредегар";
+            }
+            if (z == 4){
+                h[z].name = "Саша";
+            }
+            if (z == 5){
+                h[z].name = "Ариша";
             }
             System.out.print (h[z].name + " - ");
             System.out.println ("имя хорошего хоббита");
