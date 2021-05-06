2ac023906a31c4609d14568f89d56e5ad312ddda
Sergey <servitant@gmail.com>
2021-05-06 11:27:52 +0300

    HeadFirstJava

---
+++ src/main/java/com/HeadFirstJava/page118/Clock.java
@@ -0,0 +1,22 @@
+package com.HeadFirstJava.page118;
+
+public class Clock {
+    String time;
+
+    void setTime (String t) {
+        time = t;
+    }
+
+    String getTime () { //Тут не хватало String
+        return time;
+    }
+}
+
+class ClockTestDrive {
+    public static void main (String[] args) {
+        Clock c = new Clock ();
+        c.setTime ("1245");
+        String tod = c.getTime ();
+        System.out.println ("время: " + tod);
+    }
+}
