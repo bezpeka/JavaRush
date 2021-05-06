af7846d0b5b5d050efa1886f1fb6a2d04e2c2497
Sergey <servitant@gmail.com>
2021-05-06 12:29:34 +0300

    HeadFirstJava

---
+++ src/main/java/com/HeadFirstJava/page100to199/page121/Puzzle4.java
@@ -0,0 +1,35 @@
+package com.HeadFirstJava.page100to199.page121;
+
+public class Puzzle4 {
+    public static void main (String[] args) {
+        Puzzle4b[] obs = new Puzzle4b[6];           //пропущенная строка
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        while (x < 6) {
+            obs[x] = new Puzzle4b ();               //пропущенная строка
+            obs[x].ivar = y;                        //пропущенная строка
+            y = y * 10;
+            x = x + 1;                              //пропущенная строка
+        }
+        x = 6;
+        while (x > 0) {
+            x = x - 1;                              //пропущенная строка
+            result = result + obs[x].doStuff (x);   //пропущенная строка
+        }
+        System.out.println ("Результат " + result);
+    }
+}
+
+class Puzzle4b {                                    //пропущенная строка
+    int ivar;
+
+    public int doStuff (int factor) {               //пропущенная строка
+        if (ivar > 100){
+            return ivar * factor;                   //пропущенная строка
+        }
+        else {
+            return ivar * (5 - factor);             //пропущенная строка
+        }
+    }
+}
