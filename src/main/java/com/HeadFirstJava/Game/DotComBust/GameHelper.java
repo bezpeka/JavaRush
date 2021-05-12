b46c17f31d8eb2815ae52d8ff2b48826ef3832b7
Sergey <servitant@gmail.com>
2021-05-12 09:38:19 +0300

    HeadFirstJava

--- src/main/java/com/HeadFirstJava/Game/DotComBust/GameHelper.java
+++ src/main/java/com/HeadFirstJava/Game/DotComBust/GameHelper.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-//todo Нужно переписать этот класс 181 стр
-
 public class GameHelper {
     private static final String alphabet = "abcdefg";
     private int gridLength = 7;
