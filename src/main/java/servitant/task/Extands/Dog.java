735275a8a005bf990cc37d2230bcbaee804a6533
Sergey <servitant@gmail.com>
2021-05-24 16:05:02 +0300

    HeadFirstJava

--- src/main/java/servitant/task/Extands/Dog.java
+++ src/main/java/servitant/task/Extands/Dog.java
@@ -2,21 +2,30 @@ package servitant.task.Extands;
 
 public class Dog extends Animals { // создал класс dog, унаследованный от animals
 
-    public String name; //Добавил классу переменную типа стринг, для хранения имени
+    private String name; //Добавил классу переменную типа стринг, для хранения имени
 
     public void sleep () { //переопределил метод "спать" унаследованный от animals для dog
         System.out.println ("Dog is sleeping");
     }
-    public Dog (){ //создал конструктор по-умолчанию, так как ниже есть перегруженный конструктор
 
+    public Dog () { //создал конструктор по-умолчанию, так как ниже есть перегруженный конструктор
     }
+
     public Dog (String name) { //создал перегруженный конструктор для указания имени
-        this.name = name;
     }
 
     public static void main (String[] args) {
         Dog A = new Dog ("Layka");
         A.sleep (); //Вызвал методы animals для ссылки А типа Dog
         A.eat ();
+        System.out.println (A.getName ());
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return name;
     }
 }
