27ef64e3fab11e2aebc8102d7525e06075d0702d
Sergey <servitant@gmail.com>
2021-05-31 14:53:50 +0300

    HeadFirstJava

---
+++ src/main/java/com/HeadFirstJava/page200to299/page285/Animal.java
@@ -0,0 +1,13 @@
+package com.HeadFirstJava.page200to299.page285;
+
+public abstract class Animal {
+    private String name; //Все потомки Animal(), включая дочерние классы будут иметь имя.
+
+    public String getName(){
+        return name;
+    }
+    public Animal (String theName){ //Конструктор, принимающий в параметрах имя,
+        name = theName;             // присваивает его переменной экземпляра name
+        System.out.println ("Create Animal");
+    }
+}
