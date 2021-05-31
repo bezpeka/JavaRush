150b756fee55fdc160428a4e4b9bcb3985e4d532
Sergey <servitant@gmail.com>
2021-05-31 16:44:31 +0300

    HeadFirstJava

--- src/main/java/com/javarush/task/task07/task0702/Solution.java
+++ src/main/java/com/javarush/task/task07/task0702/Solution.java
@@ -1,4 +1,23 @@
 package com.javarush.task.task07.task0702;
 
+import java.util.Scanner;
+
+/*
+Массив из строчек в обратном порядке
+*/
+
 public class Solution {
+    public static void main (String[] args) throws Exception {
+        //напишите тут ваш код
+        String[] num = new String[10];
+
+        Scanner sc = new Scanner (System.in);
+
+        for (int i = 0; i < 8; i++){
+            num[i] = sc.nextLine ();
+        }
+        for (int i = 9; i >= 0; i--){
+            System.out.println (num[i]);
+        }
+    }
 }
