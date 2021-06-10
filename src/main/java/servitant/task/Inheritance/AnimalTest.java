fda922e18346afa8f628a93c9899bdddf6237ec1
Sergey <servitant@gmail.com>
2021-06-10 17:05:44 +0300

    HeadFirstJava

--- src/main/java/servitant/task/Inheritance/AnimalTest.java
+++ src/main/java/servitant/task/Inheritance/AnimalTest.java
@@ -1,11 +1,13 @@
 package servitant.task.Inheritance;
 
-public class animalTest {
+public class AnimalTest {
     static Dog sharik = new Dog();
     static Cat murka = new Cat();
+    static Animal Noname = new Animal();
 
     public static void main(String[] args) {
         sharik.runs();
         murka.sleep();
+        Noname.eat();
     }
 }
