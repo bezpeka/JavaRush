20772b9508dac8b52e98d42ec91f540cb6878a6b
Sergey <servitant@gmail.com>
2021-06-14 15:24:04 +0300

    HeadFirstJava

--- src/main/java/com/javarush/task/task07/task0718/Solution.java
+++ src/main/java/com/javarush/task/task07/task0718/Solution.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
 
 /* 
 Проверка на упорядоченность
+1. Введи с клавиатуры 10 слов в список строк.
+2. Определить, является ли список упорядоченным по возрастанию длины строки.
+3. В случае отрицательного ответа вывести на экран индекс первого элемента, нарушающего такую упорядоченность.
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
         //напишите тут ваш код
-        ArrayList <String> lst = new ArrayList <>();
+        ArrayList <String> list = new ArrayList <>();
         Scanner sc = new Scanner(System.in);
         for(int i = 0; i < 10; i++) {
-            lst.add(sc.nextLine());
+            list.add(sc.nextLine());
         }
-        for(int i = 0; i < lst.size() - 1; i++) {
-            if(lst.get(i).length() > lst.get(i + 1).length()){
-                System.out.println(i + 1);
+        for(int item = 0; item < list.size() - 1; item++) {
+            if(list.get(item).length() > list.get(item + 1).length()){
+                System.out.println(item + 1);
                 break;
             }
         }
