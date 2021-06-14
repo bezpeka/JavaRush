4ffbddc0c94407963c228be2a1960d127da9febe
Sergey <servitant@gmail.com>
2021-06-14 15:31:45 +0300

    HeadFirstJava

--- src/main/java/com/javarush/task/task07/task0719/Solution.java
+++ src/main/java/com/javarush/task/task07/task0719/Solution.java
@@ -7,18 +7,21 @@ import java.util.ArrayList;
 
 /* 
 Вывести числа в обратном порядке
+Ввести с клавиатуры 10 чисел и заполнить ими список.
+Вывести их в обратном порядке. Каждый элемент - с новой строки.
+Использовать только цикл for.
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         //напишите тут ваш код
-        ArrayList <Integer> list = new ArrayList <>();
+        ArrayList <Integer> list = new ArrayList <>(); //Объявили переменную типа список целых чисел и сразу ее проинициализировали
         for(int itemInput = 0; itemInput < 10; itemInput++) {
-            list.add(Integer.parseInt(reader.readLine()));
+            list.add(Integer.parseInt(reader.readLine())); //Считали 10 целых чисел с клавиатуры и добавили их в список
         }
         for(int itemOutput = list.size() - 1; itemOutput >= 0; itemOutput--) {
-            System.out.println(list.get(itemOutput));
+            System.out.println(list.get(itemOutput)); //вывели числа на экран в обратном порядке
         }
     }
 }
--- src/main/java/com/javarush/task/task07/task0720/Solution.java
+++ src/main/java/com/javarush/task/task07/task0720/Solution.java
@@ -7,31 +7,28 @@ import java.util.ArrayList;
 
 /* 
 Перестановочка подоспела
+Ввести с клавиатуры 2 числа N и M.
+Ввести N строк и заполнить ими список.
+Переставить M первых строк в конец списка.
+Вывести список на экран, каждое значение с новой строки.
+
+Примечание: запрещено создавать больше одного списка.
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         //напишите тут ваш код
-        ArrayList<String> lst = new ArrayList<>();
-
+        ArrayList <String> list = new ArrayList <>();
         int N = Integer.parseInt(reader.readLine());
         int M = Integer.parseInt(reader.readLine());
-
-        for (int i=0; i<N; i++)
-            lst.add(reader.readLine());
-
-        for (int i=0; i<M; i++)
-            lst.add (lst.get(i));
-
-        for (int i=M-1; i>=0; i--)
-            lst.remove (i);
-
-        for (String s : lst)
+        for(int i = 0; i < N; i++)
+            list.add(reader.readLine());
+        for(int i = 0; i < M; i++)
+            list.add(list.get(i));
+        for(int i = M - 1; i >= 0; i--)
+            list.remove(i);
+        for(String s: list)
             System.out.println(s);
-
-
-
     }
 }
