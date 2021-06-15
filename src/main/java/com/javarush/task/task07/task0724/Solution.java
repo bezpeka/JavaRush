20204f42449d2bab0c5337f8486ce4c4bd242d1f
Sergey <servitant@gmail.com>
2021-06-15 17:03:52 +0300

    HeadFirstJava

--- src/main/java/com/javarush/task/task07/task0724/Solution.java
+++ src/main/java/com/javarush/task/task07/task0724/Solution.java
@@ -20,17 +20,17 @@ public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
 
-        Human grandfather1 = new Human ("Bob",true,70);
-        Human grandfather2 = new Human ("Bobik",true,80);
-        Human grandmother1 = new Human ("Alisa",false,60);
-        Human grandmother2 = new Human ("Sharik",false,50);
+        Human grandfather1 = new Human("Дедушка мамы", true, 70);
+        Human grandfather2 = new Human("Дедушка папы", true, 80);
+        Human grandmother1 = new Human("Бабушка мамы", false, 60);
+        Human grandmother2 = new Human("Бабушка папы", false, 50);
 
-        Human mother = new Human ("Elka",false,20, grandfather1, grandmother1);
-        Human father = new Human ("Hren",true,30, grandfather2, grandmother2);
+        Human mother = new Human("Мама", false, 20, grandfather1, grandmother1);
+        Human father = new Human("Папа", true, 30, grandfather2, grandmother2);
 
-        Human child1 = new Human ("Redka",false,2, mother, father);
-        Human child2 = new Human ("Morkovka",true,3, mother, father);
-        Human child3 = new Human ("Repka",false,4, mother, father);
+        Human child1 = new Human("Младшая дочь", false, 2, mother, father);
+        Human child2 = new Human("Сын", true, 3, mother, father);
+        Human child3 = new Human("Старшая дочь", false, 4, mother, father);
 
         System.out.println(grandfather1);
         System.out.println(grandfather2);
@@ -43,7 +43,7 @@ public class Solution {
         System.out.println(child3);
     }
 
-    public static class Human {
+    public static class Human { // создали класс с нужными полями
         //напишите тут ваш код
         String name;
         boolean sex;
@@ -51,16 +51,16 @@ public class Solution {
         Human father;
         Human mother;
 
-        public Human (String name, boolean sex, int age) {
-            this.name = name;
+        public Human(String name, boolean sex, int age) {
+            this.name = name; // создали конструктор для класса с отсутствующими полями отец и мать
             this.sex = sex;
             this.age = age;
             this.father = null;
             this.mother = null;
         }
 
-        public Human (String name, boolean sex, int age,  Human father, Human mother) {
-            this.name = name;
+        public Human(String name, boolean sex, int age, Human father, Human mother) {
+            this.name = name; // создали конструктор для класса с полями отец и мать
             this.sex = sex;
             this.age = age;
             this.father = father;
@@ -72,13 +72,10 @@ public class Solution {
             text += "Имя: " + this.name;
             text += ", пол: " + (this.sex ? "мужской" : "женский");
             text += ", возраст: " + this.age;
-
-            if (this.father != null)
+            if(this.father != null)
                 text += ", отец: " + this.father.name;
-
-            if (this.mother != null)
+            if(this.mother != null)
                 text += ", мать: " + this.mother.name;
-
             return text;
         }
     }
