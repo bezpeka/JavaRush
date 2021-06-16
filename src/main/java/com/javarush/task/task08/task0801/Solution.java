d5340ff535c6242643f68851143e17c23e07ea8d
Sergey <servitant@gmail.com>
2021-06-16 13:39:33 +0300

    HeadFirstJava

--- src/main/java/com/javarush/task/task08/task0801/Solution.java
+++ src/main/java/com/javarush/task/task08/task0801/Solution.java
@@ -2,27 +2,44 @@ package com.javarush.task.task08.task0801;
 
 /* 
 HashSet из растений
+
+Создать коллекцию Set (реализация HashSet) с типом элементов String.
+Добавить в неё следующие строки:
+арбуз
+банан
+вишня
+груша
+дыня
+ежевика
+женьшень
+земляника
+ирис
+картофель
+
+Вывести содержимое коллекции на экран, каждый элемент с новой строки.
+Посмотреть, как изменился порядок добавленных элементов.
 */
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        HashSet hs = new HashSet<>();
+        Set <String> hs = new HashSet <>();
         hs.add("арбуз");
         hs.add("банан");
         hs.add("вишня");
         hs.add("груша");
         hs.add("дыня");
         hs.add("ежевика");
-        hs.add("жень-шень");
+        hs.add("женьшень");
         hs.add("земляника");
         hs.add("ирис");
         hs.add("картофель");
 
-        for (Object o:  hs) {
-            System.out.println((String)o );
+        for(String items: hs) {
+            System.out.println(items);
         }
     }
 }
--- src/main/java/com/javarush/task/task08/task0802/Solution.java
+++ src/main/java/com/javarush/task/task08/task0802/Solution.java
@@ -2,6 +2,22 @@ package com.javarush.task.task08.task0802;
 
 /* 
 HashMap из 10 пар
+
+Создать коллекцию Map<String, String> (реализация HashMap), занести туда 10 пар строк:
+арбуз - ягода,
+банан - трава,
+вишня - ягода,
+груша - фрукт,
+дыня - овощ,
+ежевика - куст,
+жень-шень - корень,
+земляника - ягода,
+ирис - цветок,
+картофель - клубень.
+Вывести содержимое коллекции на экран, каждый элемент с новой строки.
+
+Пример вывода (тут показана только одна строка):
+картофель - клубень
 */
 
 import java.util.HashMap;
@@ -10,23 +26,21 @@ import java.util.Map;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        HashMap<String,String> hm = new HashMap<>();
+        Map <String, String> hm = new HashMap <>();
 
-        hm.put("арбуз","ягода");
-        hm.put("банан","трава");
-        hm.put("вишня","ягода");
-        hm.put("груша","фрукт");
-        hm.put("дыня","овощ");
-        hm.put("ежевика","куст");
-        hm.put("жень-шень","корень");
-        hm.put("земляника","ягода");
-        hm.put("ирис","цветок");
-        hm.put("картофель","клубень");
+        hm.put("арбуз", "ягода");
+        hm.put("банан", "трава");
+        hm.put("вишня", "ягода");
+        hm.put("груша", "фрукт");
+        hm.put("дыня", "овощ");
+        hm.put("ежевика", "куст");
+        hm.put("жень-шень", "корень");
+        hm.put("земляника", "ягода");
+        hm.put("ирис", "цветок");
+        hm.put("картофель", "клубень");
 
-        for (Map.Entry entry: hm.entrySet()) {
-            System.out.println(entry.getKey()+" - "+entry.getValue());
+        for(Map.Entry entry: hm.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
         }
-
-
     }
 }
