5109f6770c4132e4165a473ac6bcde745d338c0f
Sergey <servitant@gmail.com>
2021-06-16 14:32:43 +0300

    HeadFirstJava

--- src/main/java/com/javarush/task/task08/task0803/Solution.java
+++ src/main/java/com/javarush/task/task08/task0803/Solution.java
@@ -5,27 +5,32 @@ import java.util.Map;
 
 /* 
 Коллекция HashMap из котов
+
+Есть класс Cat с полем имя (name, String).
+Создать коллекцию Map<String, Cat> (реализация HashMap).
+Добавить в коллекцию 10 котов, в качестве ключа использовать имя кота.
+Вывести результат на экран, каждый элемент с новой строки.
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         String[] cats = new String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
 
-        HashMap<String, Cat> map = addCatsToMap(cats);
+        Map <String, Cat> map = addCatsToMap(cats);
 
-        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+        for(Map.Entry <String, Cat> pair: map.entrySet()) {
             System.out.println(pair.getKey() + " - " + pair.getValue());
         }
     }
 
 
-    public static HashMap<String, Cat> addCatsToMap(String[] cats) {
+    public static HashMap <String, Cat> addCatsToMap(String[] cats) {
         //напишите тут ваш код
-        HashMap<String, Cat> map = new HashMap<String, Cat>();
-        for (int i=0; i<cats.length; i++) {
-            map.put(cats[i], new Cat (cats[i]));
+        Map <String, Cat> map = new HashMap <>();
+        for(String cat: cats) {
+            map.put(cat, new Cat(cat));
         }
-        return map;
+        return ( HashMap <String, Cat> ) map;
     }
 
 
