0e9136ee1e94374aff1e61178a36503ecdad3418
Sergey <servitant@gmail.com>
2021-06-16 15:09:06 +0300

    HeadFirstJava

--- src/main/java/com/javarush/task/task08/task0807/Solution.java
+++ src/main/java/com/javarush/task/task08/task0807/Solution.java
@@ -3,21 +3,21 @@ package com.javarush.task.task08.task0807;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-/* 
+/*
 LinkedList и ArrayList
 */
 
 public class Solution {
     public static Object createArrayList() {
         //напишите тут ваш код
-        ArrayList lst = new ArrayList();
-        return lst;
+        ArrayList list = new ArrayList();
+        return list;
     }
 
     public static Object createLinkedList() {
         //напишите тут ваш код
-        LinkedList lst = new LinkedList();
-        return lst;
+        LinkedList list = new LinkedList();
+        return list;
     }
 
     public static void main(String[] args) {
