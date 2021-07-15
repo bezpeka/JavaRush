a2c92fa131400b019a42bffe912f20bb12fa0c89
Sergey <servitant@gmail.com>
2021-07-15 14:59:28 +0300

    HeadFirstJava

--- src/main/java/testImport/p4/D.java
+++ src/main/java/testImport/p4/D.java
@@ -6,20 +6,28 @@ import testImport.p1.p2.p3.C;
 import testImport.p4.p5.E;
 
 public class D {
-    public void showinfo(){
+    public void showinfo() {
         System.out.println("object D is created");
     }
+
     public static void main(String[] args) {
-        A a = new A();
-        a.showinfo();
-        B b = new B();
-        b.showinfo();
-        C c = new C();
-        c.showinfo();
-        D d = new D();
-        d.showinfo();
-        E e = new E();
-        e.showinfo();
-        System.out.println(testImport.p1.p2.B.x + testImport.p1.p2.B.y);
+        test t = new test();
+        t.check();
+    }
+
+    static class test {
+        public void check() {
+            A a = new A();
+            a.showinfo();
+            B b = new B();
+            b.showinfo();
+            C c = new C();
+            c.showinfo();
+            D d = new D();
+            d.showinfo();
+            E e = new E();
+            e.showinfo();
+            System.out.println(testImport.p1.p2.B.x + testImport.p1.p2.B.y);
+        }
     }
 }
