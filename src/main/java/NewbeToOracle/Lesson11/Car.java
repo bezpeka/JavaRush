e1ebf39d117376a86568cac3a526a2c30dfdc5c7
Sergey <servitant@gmail.com>
2021-07-16 11:21:18 +0300

    HeadFirstJava

--- src/main/java/NewbeToOracle/Lesson11/Car.java
+++ src/main/java/NewbeToOracle/Lesson11/Car.java
@@ -3,21 +3,24 @@ package NewbeToOracle.Lesson11;
 public class Car {
     String color;
     String engine;
-    int door;
+    int doorCount;
 
-    public Car(String color, String engine, int door) {
+    public Car(String color, String engine, int doorCount) {
         this.color = color;
         this.engine = engine;
-        this.door = door;
+        this.doorCount = doorCount;
     }
 }
 
 class CarTest {
-    public static int changedoor(int door, int plus) {
+    public static int changeDoor(int door, int plus) {
         return door + plus;
     }
+    public static void changeDoor(Car c, int doorCount){
+        c.doorCount = doorCount;
+    }
 
-    public static void changecolor(Car c1, Car c2) {
+    public static void changeColor(Car c1, Car c2) {
         Car c3 = new Car("white", "4", 4);
         Car c4 = new Car("orange", "4", 4);
         c3.color = c1.color;
@@ -25,17 +28,27 @@ class CarTest {
         c1.color = c4.color;
         c2.color = c3.color;
     }
+    static void changeColor2(Car c1, Car c2){
+        String color = c1.color;
+        c1.color = c2.color;
+        c2.color = color;
+    }
 
     public static void main(String[] args) {
+        CarTest ct = new CarTest();
         Car c1 = new Car("red", "2", 3);
         Car c2 = new Car("blue", "3", 5);
-        changecolor(c1, c2);
+        changeColor2(c1, c2);
         System.out.println("Color c1 was red, and now is " + c1.color);
         System.out.println("Color c2 was blue, and now is " + c2.color);
-        c1.door = changedoor(c1.door, 3);
-        c2.door = changedoor(c2.door, 9);
-        System.out.println(c1.door);
-        System.out.println(c2.door);
+        c1.doorCount = changeDoor(c1.doorCount, 3);
+        c2.doorCount = changeDoor(c2.doorCount, 9);
+        System.out.println(c1.doorCount);
+        System.out.println(c2.doorCount);
+        ct.changeDoor(c1, 3);
+        ct.changeDoor(c2, 4);
+        System.out.println(c1.doorCount);
+        System.out.println(c2.doorCount);
     }
 }
 
