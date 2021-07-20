d736402fb20380a766a32d4ac2fdb00cade94579
Sergey <servitant@gmail.com>
2021-07-20 11:20:14 +0300

    HeadFirstJava

--- src/main/java/NewbeToOracle/Lesson12/StudentTest.java
+++ src/main/java/NewbeToOracle/Lesson12/StudentTest.java
@@ -4,9 +4,9 @@ import NewbeToOracle.Lesson11.Student;
 
 public class StudentTest {
     public static void comparison(Student s1, Student s2) {
-        if(s1 == s2){
-            System.out.println("Студенты равны");
-        } else System.out.println("Студенты не равны");
+        if(s1.name.equals(s2.name) && s1.course == s2.course && s1.grade == s2.grade){
+            System.out.println("Студенты одинаковые");
+        } else System.out.println("Студенты не одинаковые");
     }
 
     public static void compareNested(Student s1, Student s2) {
@@ -20,9 +20,9 @@ public class StudentTest {
 
     public static void compareNested1(Student s1, Student s2) {
         if(s1.name.equals(s2.name)){
-            System.out.println("Имена студентов одинаковые");
+            System.out.println("Имена у студентов одинаковые");
         } else {
-            System.out.println("Имена студентов разные");
+            System.out.println("Имена у студентов разные");
         }
         if(s1.course == s2.course){
             System.out.println("Курсы у студентов одинаковые");
@@ -36,12 +36,31 @@ public class StudentTest {
         }
     }
 
+    public static void compareNested2(Student s1, Student s2) {
+        if(s1.name.equals(s2.name)){
+            if(s1.course == s2.course){
+                if(s1.grade == s2.grade){
+                    System.out.println("Имена студентов, курсы и оценки одинаковые");
+                } else {
+                    System.out.println("Имена студентов и курсы одинаковые, но оценки разные");
+                }
+            } else {
+                System.out.println("Имена одинаковые, но курсы  и оценки у студентов разные");
+            }
+        } else {
+            System.out.println("Имена студентов, курсы и оценки разные");
+        }
+    }
+
+
+
     public static void main(String[] args) {
         Student st1 = new Student("Ivan", 3, 9.5);
         Student st2 = new Student("Ivan", 3, 9.5);
-        Student st3 = new Student("Petr", 1, 5.3);
-        comparison(st1, st2);
-        compareNested(st1, st3);
+        Student st3 = new Student("Petr", 3, 5.3);
+        Student st4 = new Student("Petr", 2, 5.3);
+        comparison(st1, st3);
+        compareNested2(st3, st4);
     }
 }
 
