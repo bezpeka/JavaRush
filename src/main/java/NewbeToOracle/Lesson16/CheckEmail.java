a010683bf9d4fd36d834274787eb8bc919049920
Sergey <servitant@gmail.com>
2021-07-22 10:15:46 +0300

    HeadFirstJava

---
+++ src/main/java/NewbeToOracle/Lesson16/CheckEmail.java
@@ -0,0 +1,19 @@
+package NewbeToOracle.Lesson16;
+
+public class CheckEmail {
+    public static void email(String email) {
+        int a = 0; //позиция символа @
+        int b = 0; //позиция символа .
+        int c = 0; //позиция символа ;
+        while (c < email.length() - 1) {
+            a = email.indexOf('@', c);
+            b = email.indexOf('.', c);
+            c = email.indexOf(';', c + 1);
+            System.out.println(email.substring(a + 1, b));
+        }
+    }
+
+    public static void main(String[] args) {
+        email("ya@yahoo.com; you@mail.ru; we@gmail.com;");
+    }
+}
