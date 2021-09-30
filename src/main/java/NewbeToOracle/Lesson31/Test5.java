e19c9ea86a04fd2ebdc5541050e8fed89206f305
Sergey <servitant@gmail.com>
2021-09-30 16:30:34 +0300

    HeadFirstJava

---
+++ src/main/java/NewbeToOracle/Lesson31/Test5.java
@@ -0,0 +1,14 @@
+package NewbeToOracle.Lesson31;
+
+public class Test5 {
+    static int j = 5;
+
+    static void abc(int i) {
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        abc(j++);
+        System.out.println(j);
+    }
+}
