4413e1308ab92058a89513df6b22f7d63e510c1e
Sergey <servitant@gmail.com>
2021-10-13 15:56:41 +0300

    HeadFirstJava

---
+++ src/main/java/NewbeToOracle/Lesson31/Test20.java
@@ -0,0 +1,18 @@
+package NewbeToOracle.Lesson31;
+
+public class Test20 {
+}
+
+class Tiger20 {
+    static int counter = 0;
+}
+
+class TestTiger20 {
+    public static void main(String[] args) {
+        boolean flag = false;
+        while (flag = true) {
+            Tiger20.counter++;
+        }
+        System.out.println(Tiger20.counter);
+    }
+}
